3e5dc9a558031653884bf8be4cd1342b837f0765
devhopper <devhopperbr@gmail.com>
2019-06-10 10:49:00 -0300

    projeto-esapiens https

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://api.github.com' // Url base da aplicação
+    baseURL: 'https://api.github.com' // Url base da aplicação
 });
 
 
